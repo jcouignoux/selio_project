1e91a02d2a88f7f4973cd0c7c63b4460a33e7e25
Julien Couignoux <j.couignoux@gmail.com>
2021-09-03 16:21:50 +0200

    update booking with booking_detail

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,9 +1,13 @@
 
-from .models import Booking, Categorie, Author, Publisher
+from .models import Booking, BookingDetail, Categorie, Author, Publisher
 
 def get_booking(request):
     bookings_list= Booking.objects.filter(contacted=False).order_by('created_at')
-    return {'bookings_list': bookings_list}
+    booking_detail_list = BookingDetail.objects.all()
+    return {
+        'bookings_list': bookings_list,
+        'booking_detail_list': booking_detail_list,
+        }
 
 def get_categories(request):
     categories_list= Categorie.objects.all().order_by('name')
