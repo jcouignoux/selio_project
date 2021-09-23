0c37b04d10e0d4a0a49dee17bf0d38b75725e87e
Julien Couignoux <j.couignoux@gmail.com>
2021-09-23 18:06:14 +0200

    add send mail

--- selio_project/settings.py
+++ selio_project/settings.py
@@ -163,9 +163,17 @@ if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 # else:
-#    MEDIA_ROOT = os.path.join(BASE_DIR, 'store/media/')
+#    STATIC_ROOT = os.path.join(BASE_DIR, 'store/static/')
 
-
+# default mail configuration
+# EMAIL_HOST_USER = 'j.couignoux@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Jul13n76!'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'j.couignoux@gmail.com'
+EMAIL_HOST_PASSWORD = 'Jul13n76!'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- store/store.py
+++ store/store.py
@@ -1,6 +1,11 @@
 from django.shortcuts import get_object_or_404
+from django.core.mail import EmailMessage, EmailMultiAlternatives, send_mail
+from django.template.loader import get_template
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.contrib.auth.models import User
 
-from .models import History, Ouvrage
+from .models import History, Ouvrage, Booking
 
 def add_to_history(ouvrage_id, quantity, date):
     ouvrage = get_object_or_404(Ouvrage, id=ouvrage_id)
@@ -23,3 +28,27 @@ def add_to_history(ouvrage_id, quantity, date):
         pass
     ouvrage.save()
     return ouvrage
+
+def send_email(address, content):
+    subject = content['subject']
+    # message = content['message']
+    from_email = "no-reply@selio4.org"
+    to = [address, ]
+    bcc = [User.objects.get(username='jcouignoux').email, ]
+    # attachments = ['attachments', ]
+    auth_user = User.objects.get(username='jcouignoux').email
+    auth_password = User.objects.get(username='jcouignoux').password
+    
+    
+    html_content = render_to_string('store/email.html', {'booking': content['booking']})
+    text_content = strip_tags(html_content)
+    # d = {'username': 'username' }
+
+    # body = htmly.render(d)
+
+    # email = EmailMultiAlternatives(subject=subject, body=text_content, from_email=from_email, to=to, bcc=bcc, )
+    # email.attach_alternative(subject=subject, body=text_content, from_email=from_email, to=to, bcc=bcc, )
+    response = send_mail(subject, html_content, from_email, to, html_message=html_content)
+    # response = email.send()
+
+    return response
